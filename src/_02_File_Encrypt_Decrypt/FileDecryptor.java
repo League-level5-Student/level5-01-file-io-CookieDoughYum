40b7ae9125550aea71e39553d231f539e5f95a11
root <root@a8a6963c5c23>
2021-03-15 23:55:50 +0000

    Working on FileDecryptor

--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,5 +1,12 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JFileChooser;
+
 public class FileDecryptor {
 	/*
 	 * Decryption is the process of taking encoded or encrypted text or other data
@@ -19,4 +26,22 @@ public class FileDecryptor {
 	 * Create a program that opens the file created by FileEncryptor and decrypts
 	 * the message, then display it to the user in a JOptionPane.
 	 */
+	public static void main(String[] args) {
+		ArrayList<Character> decryptedCharacters=new ArrayList<Character>();
+		try {
+			FileReader fr = new FileReader("src/_02_File_Encrypt_Decrypt/EncryptedMessage.txt");
+			int c = fr.read();
+			while(c != -1){
+				decryptedCharacters.add(char(c));
+				c = fr.read();
+			}
+			fr.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,5 +1,11 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	/*
 	 * Encryption is the process of encoding a message or information
@@ -20,4 +26,34 @@ public class FileEncryptor {
 	 * Create a program that takes a messager.
 	 * Use any key you want (1 - 25) to shift each letter in the users input and save the final result to a file.
 	 */
+	public static void main(String[] args) {
+		String message=JOptionPane.showInputDialog("Put a message to encrypt");
+		ArrayList<Character> charMessage=new ArrayList<Character>();
+		ArrayList<Character> messageEncrypt=new ArrayList<Character>();
+		for(int i=0; i<message.length(); i++) {
+			charMessage.add(message.charAt(i));
+		}
+		for(int i=0; i<charMessage.size(); i++) {
+			char c=charMessage.get(i);
+			try {
+			c=message.charAt(i+4);
+			}
+			catch(IndexOutOfBoundsException e) {
+				c=message.charAt(java.lang.Math.abs(((charMessage.size()-1)-i)-3));
+			}
+			messageEncrypt.add(c);
+		}
+		JOptionPane.showMessageDialog(null, " Your encrypted message is " + messageEncrypt);
+		char[] encryptedCharacters=new char[messageEncrypt.size()];
+		for(int i=0; i<messageEncrypt.size(); i++) {
+			encryptedCharacters[i]=messageEncrypt.get(i);
+		}
+		try {
+			FileWriter fw = new FileWriter("src/_02_File_Encrypt_Decrypt/EncryptedMessage.txt", false);
+			fw.write(encryptedCharacters);	
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
